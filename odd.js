7d8f4b58e1d3d3420b80d4ff1443de8ff5d8a003
Abdullah Habes <abdulla096@hotmail.com>
2021-10-06 23:52:00 -0400

    add odd checker function

---
+++ odd.js
@@ -0,0 +1,16 @@
+const isOdd = function (num) {
+
+    return (num % 2 === 1)
+
+}
+
+
+num_1 = 3
+num_2 = 8
+num_3 = 12
+num_4 = 23
+
+console.log(num_1 + " is odd: " + isOdd(num_1));
+console.log(num_2 + " is odd: " + isOdd(num_2));
+console.log(num_3 + " is odd: " + isOdd(num_3));
+console.log(num_4 + " is odd: " + isOdd(num_4));
